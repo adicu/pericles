e4bc695d6fa7d0ff9f5dddcbf88a578c41187155
Zhehao Mao <zhehao.mao@gmail.com>
2012-09-09 22:18:28 -0400

    first commit

---
+++ example-settings.py
@@ -0,0 +1,18 @@
+GCAL_USERNAME="somebody@example.com"
+GCAL_PASSWORD="password"
+GCAL_ID="default"
+
+MC_API_KEY="api-key-here"
+MC_LIST_ID="list-id-here"
+MC_EMAIL="somebody@example.com"
+MC_FROM_NAME="Joe Somebody"
+MC_TO_NAME="Joe's ListServ"
+
+EVENT_TEMPLATE = ("{title}\n"
+                 "{date}, {start} - {end} at {location}\n\n"
+                 "{description}")
+
+SUBJECT_TEMPLATE = "Test Newsletter %m/%d/%Y"
+
+DATE_FORMAT = '%A, %d/%m'
+TIME_FORMAT = '%H:%M %p'
---
+++ pericles.py
@@ -0,0 +1,67 @@
+import settings
+from gdata.calendar.client import CalendarClient, CalendarEventQuery
+from datetime import datetime, timedelta
+from mailsnake import MailSnake
+
+def parse_timestamp(ts):
+    daystr, timestr = tuple(ts.split('T'))
+    year, month, day = tuple([int(part) for part in daystr.split('-')])
+    hour, minute = tuple([int(part) for part in timestr.split(':')[:2]])
+    
+    return datetime(year, month, day, hour, minute)
+
+def event_text(event):
+    event_dict = {}
+
+    event_dict['title'] = event.title.text
+
+    start_time = parse_timestamp(event.when[0].start)
+    end_time = parse_timestamp(event.when[0].end)
+
+    event_dict['date'] = start_time.strftime(settings.DATE_FORMAT)
+    event_dict['start'] = start_time.strftime(settings.TIME_FORMAT)
+    event_dict['end'] = end_time.strftime(settings.TIME_FORMAT)
+
+    event_dict['location'] = event.where[0].value
+
+    event_dict['description'] = event.content.text
+
+    return settings.EVENT_TEMPLATE.format(**event_dict)
+
+
+def get_events():
+    client = CalendarClient(source='adicu-pericles-v1')
+    client.ClientLogin(settings.GCAL_USERNAME, 
+                       settings.GCAL_PASSWORD, 
+                       client.source)
+
+    uri = "https://www.google.com/calendar/feeds/" + settings.GCAL_ID + \
+                "/private/full"
+
+    start_date = datetime.today()
+    end_date = start_date + timedelta(weeks=1)
+
+    query = CalendarEventQuery()
+    query.start_min = start_date.strftime('%Y-%m-%d')
+    query.start_max = end_date.strftime('%Y-%m-%d')
+
+    return client.GetCalendarEventFeed(uri=uri, q=query)
+
+def gen_email_text():
+    feed = get_events()
+    text = '\n\n'.join([event_text(event) for event in reversed(feed.entry)])
+    return text
+
+def create_campaign(text):
+    mc = MailSnake(settings.MC_API_KEY)
+    options = {
+        "list_id" : settings.MC_LIST_ID,
+        "subject" : datetime.today().strftime(settings.SUBJECT_TEMPLATE),
+        "from_email" : settings.MC_EMAIL,
+        "from_name" : settings.MC_FROM_NAME,
+        "to_name" : settings.MC_TO_NAME,
+    }
+    mc.campaignCreate(type='regular', content={'text': text}, options=options)
+
+if __name__ == '__main__':
+    create_campaign(gen_email_text())
