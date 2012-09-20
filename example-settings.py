95faedf2b1c0104a1e0fa1aa14a84f363ff6d52c
Zhehao Mao <zhehao.mao@gmail.com>
2012-09-20 12:51:19 -0400

    modify example-settings.py

--- example-settings.py
+++ example-settings.py
@@ -7,10 +7,12 @@ MC_LIST_ID="list-id-here"
 MC_EMAIL="somebody@example.com"
 MC_FROM_NAME="Joe Somebody"
 MC_TO_NAME="Joe's ListServ"
+MC_TEMPLATE_SECTION="std_content00"
+MC_TEMPLATE_NAME="Joes Template"
 
-EVENT_TEMPLATE = ("{title}\n"
-                 "{date}, {start} - {end} at {location}\n\n"
-                 "{description}")
+EVENT_TEMPLATE = ("<h3>{title}</h3>\n"
+                 "<h4>{date}, {start} - {end} at {location}</h4>\n"
+                 "<p>{description}</p>")
 
 SUBJECT_TEMPLATE = "Test Newsletter %m/%d/%Y"
 
