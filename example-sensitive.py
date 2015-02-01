6ee2cb94283bd4cc2a740ead4af3af440697ace1
jettca <jettca1@gmail.com>
2015-02-01 16:27:45 -0500

    Updated to calendar v3 API

---
+++ example-sensitive.py
@@ -0,0 +1,18 @@
+# this comes from google calendar settings
+GCAL_ID="blahblahblah@group.google.calendar.com"
+
+# these come from google app settings (email jettca1 -at- gmail -dot- com for more detail)
+GCAL_CLIENT_ID=""
+GCAL_CLIENT_SECRET=""
+
+# these come from mailchimp account settings (email same address)
+MC_API_KEY=""
+MC_LIST_ID=""
+
+# just leave these it's fine
+MC_LIST_NAME="ADI Newsletter"
+MC_EMAIL="dina@adicu.com"
+MC_FROM_NAME="Application Development Initiative"
+MC_TO_NAME="ADI Newsletter"
+MC_TEMPLATE_SECTION="std_content00"
+MC_TEMPLATE_NAME="ADI Newsletter"
