d1f04f36489e4cd6ca55d4ab08a5a42b031336f1
jettca <jettca1@gmail.com>
2015-04-25 15:41:12 -0400

    everything autogenerates nicely

--- pericles.py
+++ pericles.py
@@ -1,22 +1,22 @@
 import argparse
 import httplib2
 from datetime import datetime, timedelta
+import requests
+import json
+import fileinput
 
+from sensitive import *
 import settings
+
+from bson import json_util
 
 from mailsnake import MailSnake
 
-import gflags
-from oauth2client.file import Storage
+import gspread
+
 from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.tools import run_flow
 from oauth2client import tools
-from apiclient.discovery import build
-
-import requests
-
-from bson import json_util
-import json
+from oauth2client.file import Storage
 
 def find_template(name):
     mc = MailSnake(settings.MC_API_KEY)
@@ -36,39 +36,18 @@ def find_list(name):
         return 0
 
     return lists['data'][0]['id']
-
-def toTimeString(dateTime):
-    # formatting example: 2011-06-03T10:00:00-07:00
-
-    month = str(dateTime.month)
-    if len(month) == 1:
-        month = '0' + month
-
-    day = str(dateTime.day)
-    if len(day) == 1:
-        day = '0' + day
-
-    return str(dateTime.year) + '-' + month + '-' + day + 'T00:00:00-05:00'
-
-def fromTimeString(ts):
-    if 'T' in ts:
-        daystr, timestr = tuple(ts.split('T'))
-    else:
-        daystr = ts
-        timestr = None
-    year, month, day = tuple([int(part) for part in daystr.split('-')])
-    if timestr:
-        hour, minute = tuple([int(part) for part in timestr.split(':')[:2]])
-    else:
-        return datetime(year, month, day), False
-    return datetime(year, month, day, hour, minute), True
+ 
+def fromTimeString(dts):
+    [ds, ts] = dts.split(' ')
+    [month, day, year] = map(int, ds.split('/'))
+    [hour, minute, second] = map(int, ts.split(':'))
+    return datetime(year, month, day, hour, minute)
 
 def event_text(event, html=True):
-    ''' convert event from Google format to Mailchimp format '''
 
-    # TODO: fix start_set, end_set
     event_data = {}
     event_data['title'] = event['title']
+    event_data['url'] = event.get('facebook_url', 'http://www.adicu.com')
     
     start_time = event['start_datetime']
     end_time = event['end_datetime']
@@ -78,41 +57,95 @@ def event_text(event, html=True):
     event_data['end_time'] = end_time.strftime(settings.TIME_FORMAT)
 
     event_data['location'] = event.get('location', 'TBA')
-    event_data['description'] = event.get('description', 'No description')
+    event_data['description'] = event.get('long_description')
     
     if event_data['start_date'] != event_data['end_date']:
-        if start_set and end_set:
-            template = unicode(settings.EVENT_HTML_TEMPLATE_WITH_ALL) if html \
-                    else unicode(settings.EVENT_TEXT_TEMPLATE_WITH_ALL)
-        else:
-            template = unicode(settings.EVENT_HTML_TEMPLATE_NO_TIMES) if html \
-                    else unicode(settings.EVENT_TEXT_TEMPLATE_NO_TIMES)
+        template = unicode(settings.EVENT_HTML_TEMPLATE_WITH_ALL) if html \
+                else unicode(settings.EVENT_TEXT_TEMPLATE_WITH_ALL)
     else:
         template = unicode(settings.EVENT_HTML_TEMPLATE_DEFAULT) if html \
                 else unicode(settings.EVENT_TEXT_TEMPLATE_DEFAULT)
             
     return template.format(**event_data)
 
+def get_credentials():
+    storage = Storage('credentials.txt')
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        flow = OAuth2WebServerFlow(client_id=CLIENT_ID,
+                                   client_secret=CLIENT_SECRET,
+                                   scope='https://spreadsheets.google.com/feeds')
+        parser = argparse.ArgumentParser(parents=[tools.argparser])
+        flags = parser.parse_args()
+
+        credentials = tools.run_flow(flow, storage, flags)
+
+    return credentials
+
+def recordToEvent(record):
+    event = { 'title': record['Name of Event']
+            , 'facebook_url': record['Link to Event']
+            , 'start_datetime': fromTimeString(record['Start Time'])
+            , 'end_datetime': fromTimeString(record['End Time'])
+            , 'location': record.get('Location of Event', 'TBA')
+            , 'long_description': record['Blurb'] }
+    return event
+
+def isThisWeek(event):
+    now = datetime.now()
+    day = now.weekday()
+    sunday = now - timedelta(days=day, hours=now.hour, minutes=now.minute)
+    return sunday <= event['start_datetime'] < sunday + timedelta(days=7)
+
+def get_sheets_events():
+    gc = gspread.authorize(get_credentials())
+    sheets = gc.openall()
+    sheet = gc.open('ADI Community Events')
+    events = map(recordToEvent, sheet.sheet1.get_all_records())
+    
+    return filter(isThisWeek, events)
+
 def get_events():
-    # TODO: get google calendar events
-    # rewrite to use eventum api
-    r = requests.get('http://10.0.2.2:5000/admin/api/events/this_week')
-    return json.loads(r.text, object_hook=json_util.object_hook)
+    r = requests.get('https://adicu.com/admin/api/events/this_week')
+    eventum_events = json.loads(r.text, object_hook=json_util.object_hook)['data']
+    sheets_events = get_sheets_events()
+    return eventum_events, sheets_events
 
 def gen_blurb(html=True):
-    blurb_text = 'haha what\'s up'
+    blurb_text = ''
+    for line in fileinput.input():
+        blurb_text = blurb_text + '\n' + line
+
     if html:
         return u'<h3>Hey ADI,</h3><p>' + blurb_text + '</p><br/>'
     else:
-        return u'Hey ADI\n Have a good week'
+        return u'Hey ADI,\n' + blurb_text
+
+def gen_seperator(html=True):
+    seperator_text = '-- In The Community --'
+    if html:
+        return u'<br/><h4>' + seperator_text + '</h4><br/>'
+    else:
+        return u'\n' + seperator_text + '\n'
+
+def gen_events(events, html=True):
+    return u'\n\n'.join([event_text(event, html) 
+                        for event in events])
 
 def gen_email_text():
-    feed = get_events()
-    text = u'\n\n'.join([event_text(event, False) 
-                        for event in feed])
-    html = u'\n\n'.join([event_text(event, True) 
-                        for event in feed])
-    return gen_blurb(True) + html, gen_blurb(False) + text
+    adi, community = get_events()
+    adi_html = gen_events(adi, True)
+    adi_text = gen_events(adi, False)
+    community_html = gen_seperator(True) + gen_events(community, True) \
+            if len(community) > 0 else ''
+    community_text = gen_seperator(False) + gen_events(community, False) \
+            if len(community) > 0 else ''
+
+    html = gen_blurb(True) + adi_html + community_html
+    text = gen_blurb(False) + adi_text + community_text
+
+    return html, text
 
 def create_campaign(html, text):
     mc = MailSnake(settings.MC_API_KEY)
